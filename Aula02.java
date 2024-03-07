3d284353d1464e14fc8040cf153059b730bf1ddb
Afonso Caluege <acaluege@outlook.com>
2024-03-07 17:27:40 +0100

    teste aula02

---
+++ Aula02.java
@@ -0,0 +1,10 @@
+package com.loiane.estrutura_de_dados.vetor.teste;
+
+import com.loiane.estrutura_de_dados.vetor.Vetor;
+
+public class Aula02 {
+    
+    public static void main(String[]args) {
+        Vetor vetor = new Vetor(5);
+    }
+}
